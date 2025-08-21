36069757b3ac11a7ca98b2b7184c343354620ce9
kinibuhle <cindimosa19@gmail.com>
2025-08-21 10:48:31 +0200

    update

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ import jwt from 'jsonwebtoken';
 
 //app config
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 
 // temporary in-memory "database"
 let users = [];
